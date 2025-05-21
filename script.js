0fe76a12e120718672a2ec7b92092a91e9a233fa
iransampaio <165440003+iransampaio@users.noreply.github.com>
2025-05-21 19:53:51 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function adicionarAoCarrinho(produto) {
-    const lista = document.getElementById("lista-carrinho");
-    const item = document.createElement("li");
-    item.textContent = produto;
-    lista.appendChild(item);
-}
